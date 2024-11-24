bd233ece97f4419a1c34a7e534a1f29ccf959d5f
Lomri <varkkij@gmail.com>
2024-11-24 14:40:00 +0200

    Tehty harjoitus 5

---
+++ H5/classb.cpp
@@ -0,0 +1,15 @@
+#include "classb.h"
+
+ClassB::ClassB() {
+
+}
+
+string ClassB::getInfo() const
+{
+    return info;
+}
+
+void ClassB::setInfo(const string &newInfo)
+{
+    info = newInfo;
+}
---
+++ H5/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include "classb.h"
+#include "assosiationa.h"
+#include "aggregationa.h"
+
+using namespace std;
+
+int main()
+{
+    // Vaihe 1
+    int a = 5;
+    int b = 10;
+    cout << "a:n arvo on: " << a << " ja osoite on: " << &a << endl;
+    cout << "b:n arvo on: " << b << " ja osoite on: " << &b << endl;
+
+    // Osoitinmuuttuja myPointer
+    int *myPointer = &a;
+    cout << "Pointterin osoittama osoite on: " << myPointer << endl;
+    cout << "Pointterin osoittaman muistipaikan arvo on: " << *myPointer << endl;
+
+    myPointer = &b;
+    cout << "Pointterin osoittama osoite on: " << myPointer << endl;
+    cout << "Pointterin osoittaman muistipaikan arvo on: " << *myPointer << endl;
+
+    // Referenssimuuttuja refA
+    int &refA = a;
+    cout << "refA osoittaa osoitteeseen: " << &refA << endl;
+    cout << "refA:n osoittaman muistipaikan arvo on: " << refA << endl;
+
+    refA = b; // Toimii
+    cout << "refA osoittaa osoitteeseen: " << &refA << endl;
+    cout << "refA:n osoittaman muistipaikan arvo on: " << refA << endl;
+
+    // Vaihe 2 (Assosiation)
+    ClassB objB;
+    objB.setInfo("Olion B asettama info");
+
+    AssosiationA objAss(objB);
+    objAss.setBinfo("Olion objAss asettama info");
+
+    cout<<"Assosiaatio esimerkki:"<<endl;
+    cout<<"objB: "<<objB.getInfo()<<endl;
+    cout<<"objAss: "<<objAss.getBinfo()<<endl;
+
+    // Vaihe 3 (Aggregation)
+    cout<<"Aggregaatio esimerkki:"<<endl;
+    ClassB &refB=objB;
+    AggregationA objAggr(refB);
+    objAggr.setBinfo("Olion Agr asettama info");
+    cout<<"objB: "<<objB.getInfo()<<endl;
+    cout<<"objAggr "<<objAggr.getBinfo()<<endl;
+    cout<<endl;
+
+    return 0;
+}
